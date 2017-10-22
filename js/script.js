491de9e465e7fe5b646284df358cc15c64105abd
Margaret <30538997+margaretnatalie@users.noreply.github.com>
2017-10-22 15:51:25 -0700

    more changes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,48 @@
+console.log('fuck me up')
 $(document).ready(function(){
 
-	$("h2").click(function(){
-		setTimeout(function () {
-			alert("you are going to be alright");
-		}, 10000);
-	}, function(){
-
+	console.log('fuck me up')
+	$('.firstpage_link').on('click', function(){
+		$.alert('You are going to be alright')
+		onClose: function()
 	})
 
-});
+
+
+})
+
+
+
+// $(document).ready(function(){
+// 	  console.log('fuck me up')
+// $(document).on("click", "a", function () {
+
+//     // get the href attribute
+//     var newUrl = $(this).attr("href");
+
+//     // veryfy if the new url exists or is a hash
+//     if (!newUrl || newUrl[0] === "#") {
+//         // set that hash
+//         location.hash = newUrl;
+//         return;
+//     }
+
+
+//     // now, fadeout the html (whole page)
+//     $("html").fadeOut(function () {
+//         // when the animation is complete, set the new location
+//         location = newUrl;
+//     });
+
+//     // prevent the default browser behavior.
+//     return false;
+// });
+// 	$("h2").click(function(){
+// 		setTimeout(function () {
+// 			alert("you are going to be alright");
+// 		}, 10000);
+// 	}, function(){
+
+// 	})
+
+// });
