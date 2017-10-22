f88f3bd141f44b8e376968de358ceac842a1be05
Margaret <30538997+margaretnatalie@users.noreply.github.com>
2017-10-22 14:18:40 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-$(document).ready()
+$(document).ready(function(){
+
+	$("h2").hover(function(){
+		setTimeout(function () {
+			alert("you are going to be alright");
+		}, 10000);
+	}, function(){
+
+	})
+
+});
