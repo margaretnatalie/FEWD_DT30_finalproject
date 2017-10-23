c40026e8a99938387e3fae49e968931e85caee3d
Margaret <30538997+margaretnatalie@users.noreply.github.com>
2017-10-23 02:06:31 -0700

    changes placement of h2

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+
+
 $('#fp_distract').click( function() {
     alert('You are going to be alright. Do not think negatively!');
 	});
@@ -11,8 +13,10 @@ $('#vegan_distract').click( function() {
     alert('All of it is a work-in-progress. Do not forget to give it time. Be patient with yourself and with the process of self-improvement.');
 	});
 
+
 $('#lens_distract').click( function() {
     alert('You are awesome! Do not forget it!');
+    event.preventDefault();
 	});
 
 return false;
@@ -23,52 +27,3 @@ console.log('fuck me up')
 
 
 })
-
-// console.log('fuck me up')
-// $(document).ready(function(){
-
-// 	console.log('fuck me up')
-// 	$('.firstpage_link').on('click', function(){
-// 		$.alert('You are going to be alright')
-// 		onClose: function()
-// 	})
-
-
-
-// })
-
-
-
-// $(document).ready(function(){
-// 	  console.log('fuck me up')
-// $(document).on("click", "a", function () {
-
-//     // get the href attribute
-//     var newUrl = $(this).attr("href");
-
-//     // veryfy if the new url exists or is a hash
-//     if (!newUrl || newUrl[0] === "#") {
-//         // set that hash
-//         location.hash = newUrl;
-//         return;
-//     }
-
-
-//     // now, fadeout the html (whole page)
-//     $("html").fadeOut(function () {
-//         // when the animation is complete, set the new location
-//         location = newUrl;
-//     });
-
-//     // prevent the default browser behavior.
-//     return false;
-// });
-// 	$("h2").click(function(){
-// 		setTimeout(function () {
-// 			alert("you are going to be alright");
-// 		}, 10000);
-// 	}, function(){
-
-// 	})
-
-// });
