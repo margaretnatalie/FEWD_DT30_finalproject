48d124c0d6c026f644de8aa5be6a7e8c89b46e45
Margaret <30538997+margaretnatalie@users.noreply.github.com>
2017-10-23 00:15:37 -0700

    adding stuff

--- js/script.js
+++ js/script.js
@@ -3,11 +3,17 @@ $('#fp_distract').click( function() {
     alert('You are going to be alright. Do not think negatively!');
 	});
 
-
 $('#fp1_distract').click( function() {
     alert('Give it time. Be patient with the process of self-improvement.');
 	});
 
+$('#vegan_distract').click( function() {
+    alert('All of it is a work-in-progress. Do not forget to give it time. Be patient with yourself and with the process of self-improvement.');
+	});
+
+$('#lens_distract').click( function() {
+    alert('You are awesome! Do not forget it!');
+	});
 
 return false;
 
